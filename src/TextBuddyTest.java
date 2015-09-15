3d6fa61f761183b4babc9c99b1439ee9d66068b6
wei zheng <icedspace@gmail.com>
2015-09-15 18:21:33 +0800

    Setup up JUnit, added two junit test for the function printMessage and isOfOneArguement

---
+++ src/TextBuddyTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TextBuddyTest {
+    TextBuddy textBuddy = new TextBuddy("file.txt");
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+    
+    @Test
+    public void printMessageTest(){
+        String message = "hello";
+        TextBuddy.printMessage(message);
+        assertEquals(message + System.lineSeparator(), outContent.toString());
+    }
+    
+    @Test
+    public void arguementNotOneTest(){
+        String[] args = {"0", "1" };
+        assertFalse(TextBuddy.isOfOneArgument(args));
+    }
+
+}
